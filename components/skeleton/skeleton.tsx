16cb953b3c3599b9fd4352ea2611d76d8e4f5f6d
fireyy <fireyy@gmail.com>
2022-06-15 11:14:51 +0800

    feat: skeleton组件

---
+++ components/skeleton/skeleton.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { useClasses as clsx } from '@geist-ui/core'
+import styles from './skeleton.module.css'
+
+interface Props {
+  width?: React.CSSProperties['width']
+  height?: number
+  boxHeight?: number
+  rounded?: boolean
+  squared?: boolean
+  style?: any
+  className?: string
+  autoSize?: boolean
+  vcenter?: boolean
+  show?: boolean
+  children?: React.ReactNode
+}
+
+const isChildNull = (children: React.ReactNode) => {
+  return !React.Children.count(children)
+}
+
+const Skeleton: React.FC<Props> = ({
+  children,
+  width = 24,
+  height = 24,
+  boxHeight,
+  vcenter,
+  show,
+  ...props
+}) => {
+  return (
+    <span
+      className={clsx(styles.skeleton, {
+        [styles.show]: show !== false,
+        [styles.wrapper]: !isChildNull(children),
+        [styles.rounded]: 'rounded' in props,
+        [styles.squared]: 'squared' in props,
+      })}
+      style={
+        !isChildNull(children) || show
+          ? undefined
+          : {
+              width: width || 160,
+              minHeight: height,
+              ...(vcenter
+                ? {
+                    marginBottom: `calc(${((boxHeight || height) - height) / 2}px)`,
+                    marginTop: `calc(${((boxHeight || height) - height) / 2}px)`,
+                  }
+                : {
+                    marginBottom: `calc(${(boxHeight || height) - height}px)`,
+                  }),
+            }
+      }
+    >
+      {children}
+    </span>
+  )
+}
+
+export default Skeleton
