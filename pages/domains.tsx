ebc9eabe4d0aa0d72a348d75ce98f6281acda602
fireyy <fireyy@gmail.com>
2022-06-15 11:28:00 +0800

    feat: domains filter功能

--- pages/domains.tsx
+++ pages/domains.tsx
@@ -6,6 +6,7 @@ import Layout from 'components/layout'
 import { useForm } from 'react-hook-form'
 import useSWR from 'swr'
 import { formatDate } from 'lib/utils'
+import InputFilter from 'components/input-filter'
 
 type FormData = {
   domain: string
@@ -18,8 +19,15 @@ const Domains: NextPage = () => {
   const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>()
   const { setToast } = useToasts()
   const [loading, setLoading] = useState(false)
+  const [filterData, setFilterData] = useState([])
 
   const { data: domains, error, mutate } = useSWR(`/api/domains`)
+
+  useEffect(() => {
+    if (domains && domains.length > 0) {
+      setFilterData(domains)
+    }
+  }, [domains])
 
   const onSubmit = handleSubmit(async (data) => {
     setLoading(true)
@@ -65,12 +73,25 @@ const Domains: NextPage = () => {
       <Button type="secondary" auto scale={1/3} font="12px" onClick={() => removeDomain(rowData.domain)}>Delete</Button>
     )
   }
+  const handleFilterChange = (name: string, value: string, callback = () => {}) => {
+    if (domains && domains.length > 0) {
+      if (value) {
+        const filterResult = domains.filter((item: any) => {
+          return String(item[name]).toLowerCase().includes(value.toLowerCase())
+        })
+        setFilterData(filterResult)
+      } else {
+        setFilterData(domains)
+      }
+      callback && callback()
+    }
+  }
 
   return (
     <Layout title="Domains">
       <Grid.Container gap={2} justify="flex-start">
         <Grid md={12}>
-          <Input placeholder='domain filter' />
+          <InputFilter name="domain" onChange={handleFilterChange} />
         </Grid>
         <Grid md={12} justify="flex-end">
           <Button auto type="secondary" icon={<Plus />} onClick={() => setVisible(true)} scale={2/3}>
@@ -78,7 +99,7 @@ const Domains: NextPage = () => {
           </Button>
         </Grid>
         <Grid md={24}>
-          <Table data={domains}>
+          <Table data={filterData}>
             <Table.Column prop="domain" label="Domain" />
             <Table.Column prop="comment" label="" render={renderLinks} />
             <Table.Column prop="createdAt" label="createdAt" render={(time: string) => (<>{formatDate(time)}</>)} />
