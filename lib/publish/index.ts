30543c477a01b85614f68b6c370ec23fc5b1f61d
fireyy <fireyy@gmail.com>
2022-06-16 18:09:44 +0800

    fix: type

--- lib/publish/index.ts
+++ lib/publish/index.ts
@@ -9,8 +9,8 @@ const providers = {
 const provider = providers[process.env.STORAGE_PROVIDER || 'local']
 
 const publish = {
-  push (domain: string, list: any, mime: string) { //TODO: list type
-    return provider.put(domain, list, mime)
+  push (domain: string, content: string, mime: string) {
+    return provider.put(domain, content, mime)
   }
 }
 
